beb1a0224586db7b0554c9355bfdafe9fef372b9
mickael.forgh@gmail.com <mickael.forgh@gmail.com@68b665fa-9da4-a68a-76a0-4a3d28d06420>
2013-12-05 12:28:09 +0000

    - Ajout d'un index.php - Correction de la BDD, désormais fonctionnelle

--- trunk/bd.sql
+++ trunk/bd.sql
@@ -8,21 +8,21 @@ CREATE TABLE PARTICIPANT
    NOM CHAR(32) NOT NULL  ,
    PRENOM CHAR(32) NOT NULL  ,
    PASSWORD CHAR(100) NOT NULL,
-   MAIL CHAR(32) NOT NULL  
-   , CONSTRAINT pk_participant PRIMARY KEY (ID_PARTICIPANT) 
+   MAIL CHAR(32) NOT NULL, 
+   CONSTRAINT pk_participant PRIMARY KEY (ID_PARTICIPANT) 
  ) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
 
 
 # -----------------------------------------------------------------------------
-#       TABLE : DATE
+#       TABLE : MOMENT
 # -----------------------------------------------------------------------------
 
-CREATE TABLE DATE(
-	ID_DATE int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE MOMENT(
+   ID_DATE int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
    JOUR DATE NOT NULL,
-   TEMPS char(10)  
-	CONSTRAINT pk_date PRIMARY KEY (ID_DATE))
- ) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
+   TEMPS char(10) ,
+   CONSTRAINT pk_date PRIMARY KEY (ID_DATE)
+)  engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
 
 
 # -----------------------------------------------------------------------------
@@ -30,11 +30,11 @@ CREATE TABLE DATE(
 # -----------------------------------------------------------------------------
 
 CREATE TABLE SALLE
- (
+(
    NUM_SALLE int(3) UNSIGNED NOT NULL  ,
-   NOM_SALLE CHAR(32) NOT NULL UNIQUE,  
-   , CONSTRAINT pk_salle PRIMARY KEY (NUM_SALLE) 
- ) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
+   NOM_SALLE CHAR(32) NOT NULL UNIQUE, 
+   CONSTRAINT pk_salle PRIMARY KEY (NUM_SALLE) 
+)  engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
 
 
 # -----------------------------------------------------------------------------
@@ -42,42 +42,39 @@ CREATE TABLE SALLE
 # -----------------------------------------------------------------------------
 
 CREATE TABLE REUNION
- (
+(
    ID_REUNION int(6) UNSIGNED NOT NULL AUTO_INCREMENT ,
    ID_CHEF_REUNION INT(4) UNSIGNED NOT NULL,
    ID_DATE int(4) UNSIGNED NOT NULL ,
    SUJET CHAR(32) NULL ,
-   SALLE NOM_SALLE CHAR(32) NOT NULL,
+   SALLE CHAR(32) NOT NULL,
    INDEX (ID_CHEF_REUNION),
    INDEX (SALLE),
-   INDEX (ID_DATE)
-   , CONSTRAINT pk_reunion PRIMARY KEY (ID_REUNION) ,
-   CONSTRAINT fk_reunion_date FOREIGN KEY (ID_DATE) REFERENCES DATE(ID_DATE),
+   INDEX (ID_DATE), 
+   CONSTRAINT pk_reunion PRIMARY KEY (ID_REUNION),
+   CONSTRAINT fk_reunion_date FOREIGN KEY (ID_DATE) REFERENCES MOMENT(ID_DATE),
    CONSTRAINT fk_reunion_salle FOREIGN KEY (SALLE) REFERENCES SALLE(NOM_SALLE),
    CONSTRAINT fk_reunion_chef FOREIGN KEY (ID_CHEF_REUNION) REFERENCES PARTICIPANT(ID_PARTICIPANT)
- ) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
+) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
 
 
 # -----------------------------------------------------------------------------
 #       TABLE : PARTICIPE
 # -----------------------------------------------------------------------------
 
-CREATE PARTICIPE
- (
+CREATE TABLE PARTICIPE
+(
    NOM_SALLE CHAR(32) NOT NULL ,
    ID_REUNION int(6) UNSIGNED NOT NULL  ,
    ID_PARTICIPANT INT(4) UNSIGNED NOT NULL  ,
-   INDEX (ID_SALLE),
+   INDEX (NOM_SALLE),
    INDEX (ID_REUNION),
    INDEX (ID_PARTICIPANT),
-   CONSTRAINT pk_participe_salle PRIMARY KEY (ID_SALLE),
-   CONSTRAINT pk_participe_reun PRIMARY KEY (ID_REUNION),
-   CONSTRAINT pk_participe_part PRIMARY KEY (ID_PARTICIPANT),
+   CONSTRAINT pk_participe PRIMARY KEY (NOM_SALLE, ID_REUNION, ID_PARTICIPANT),
    CONSTRAINT fk_participe_salle FOREIGN KEY (NOM_SALLE) REFERENCES SALLE(NOM_SALLE),
    CONSTRAINT fk_participe_reun FOREIGN KEY (ID_REUNION) REFERENCES REUNION(ID_REUNION),
-   CONSTRAINT fk_participe_part FOREIGN KEY (ID_PARTICIPANT) REFERENCES PARTICIPANT(ID_PARTICIPANT),
-   
- ) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
+   CONSTRAINT fk_participe_part FOREIGN KEY (ID_PARTICIPANT) REFERENCES PARTICIPANT(ID_PARTICIPANT)
+) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
  
 
 # -----------------------------------------------------------------------------
@@ -90,10 +87,9 @@ CREATE TABLE EST_ABSENT
    ID_DATE int(4) UNSIGNED NOT NULL ,
    INDEX (ID_PARTICIPANT),
    INDEX (ID_DATE),
-   CONSTRAINT pk_est_absent_part PRIMARY KEY (ID_PARTICIPANT),
-   CONSTRAINT pk_est_absent_date PRIMARY KEY (ID_DATE),
+   CONSTRAINT pk_est_absent_part PRIMARY KEY (ID_PARTICIPANT,ID_DATE),
    CONSTRAINT fk_est_absent_part FOREIGN KEY (ID_PARTICIPANT) REFERENCES PARTICIPANT(ID_PARTICIPANT),
-   CONSTRAINT fk_est_absent_date FOREIGN KEY (ID_DATE) REFERENCES DATE(ID_DATE)
+   CONSTRAINT fk_est_absent_date FOREIGN KEY (ID_DATE) REFERENCES MOMENT(ID_DATE)
  ) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
 
 # -----------------------------------------------------------------------------
@@ -106,10 +102,9 @@ CREATE TABLE EST_DISPONIBLE
    ID_DATE int(4) UNSIGNED NOT NULL ,
    INDEX (ID_PARTICIPANT),
    INDEX (ID_DATE),
-    CONSTRAINT pk_disponible_part PRIMARY KEY (ID_PARTICIPANT),
-   CONSTRAINT pk_disponible_date PRIMARY KEY (ID_DATE),
+    CONSTRAINT pk_disponible_part PRIMARY KEY (ID_PARTICIPANT,ID_DATE),
    CONSTRAINT fk_est_disponible_part FOREIGN KEY (ID_PARTICIPANT) REFERENCES PARTICIPANT(ID_PARTICIPANT),
-   CONSTRAINT fk_est_disponible_date FOREIGN KEY (ID_DATE) REFERENCES DATE(ID_DATE)
+   CONSTRAINT fk_est_disponible_date FOREIGN KEY (ID_DATE) REFERENCES MOMENT(ID_DATE)
  ) engine=innodb CHARACTER SET UTF8 COLLATE utf8_unicode_ci;
 
 
---
+++ trunk/index.php
@@ -0,0 +1,21 @@
+﻿<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/style.css" >
+		<title> Agenda </title>
+	</head>
+	
+	<body>
+		<?php include("include/header.php"); ?>
+		<div id="bodycentered" >
+		<h1>Bienvenue sur votre Agenda</h1>
+		<?php include("controleurs/agenda.php"); ?>		
+			
+			
+		</div>
+				
+	</body>
+</html>
+
