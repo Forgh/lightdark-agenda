5ffc20551683cca26e0ed52f81d6358b7c3b6e0d
mickael.forgh@gmail.com <mickael.forgh@gmail.com@68b665fa-9da4-a68a-76a0-4a3d28d06420>
2013-12-06 16:28:27 +0000

    - Ajout d'un liste_check_membre.php qui affiche une liste par hiérarchie des membres de l'entreprise, avec des checkbox.
    ATTENTION, il faut faire du JQuery pour faire apparaitre les sous-partie au clic et du Jquery pour check les sous-liste au check du li parent.

---
+++ controleurs/liste_check_membre.php
@@ -0,0 +1,30 @@
+<?php
+	require('modeles/Equipe.php');
+	require('modeles/Service.php');
+	
+	$services = Service::getAllServices();
+?>	
+	<ul id="liste_services">
+		<?php 
+			foreach($services as $services_values){
+				?>	<li><input type="checkbox" class="service"><?php echo $services_values['NOM_SERVICE']; ?> 
+						<ul class="liste_equipes">
+							<?php
+								$equipes = Equipe::getEquipesFromService($services_values['ID_SERVICE']);
+							 	foreach ($equipes as $equipes_values) { ?>
+									 <li><input type="checkbox" class="equipe"><?php echo $equipes_values['NOM_EQUIPE']; ?> 
+									 		<ul class="liste_membres">
+									 			<?php
+									 				$membres = Equipes::getMembresEquipe($equipes_values['ID_EQUIPE']);
+													foreach ($membres as $membres_values) { ?>
+														<li><input type="checkbox" class="membre" name="<?php echo $membres_values['ID_PARTICIPANT']; ?>"><?php echo $membres_values['NOM']; echo $membres_values['PRENOM']; ?></li>
+												<?php	}
+									 			?>
+									 		</ul>	
+									 </li>
+								 <?php }
+							?>
+						</ul>
+					</li>
+		<?php	} ?>	
+	</ul>
