14ff8aa775df1e474eb8d6fc98b7ab89f715134e
mickael.forgh@gmail.com <mickael.forgh@gmail.com@68b665fa-9da4-a68a-76a0-4a3d28d06420>
2013-12-07 14:07:46 +0000

    - Correction d'erreurs dans les requêtes SQL et les choix de colonnes.

--- trunk/controleurs/liste_check_membre.php
+++ trunk/controleurs/liste_check_membre.php
@@ -4,24 +4,24 @@
 	require('modeles/Utilisateur.php');
 	
 	$services = Service::getAllServices();
-	$liste_all_membres= Utilisateurs::getListeMembres();
+	$liste_all_membres= Utilisateur::getListeMembres();
 	
-	Utilisateurs::listerMembre($liste_all_membres);
+	Utilisateur::listerMembre($liste_all_membres);
 	
 ?>
 
 	<ul id="liste_services">
 		<?php 
 			foreach($services as $services_values){
-				?>	<li><input type="checkbox" class="service"><?php echo $services_values['NOM_SERVICE']; ?> 
+				?>	<li><input type="checkbox" class="service"><?php echo $services_values['NOM']; ?> 
 						<ul class="liste_equipes">
 							<?php
 								$equipes = Equipe::getEquipesFromService($services_values['ID_SERVICE']);
 							 	foreach ($equipes as $equipes_values) { ?>
-									 <li><input type="checkbox" class="equipe"><?php echo $equipes_values['NOM_EQUIPE']; ?> 
+									 <li><input type="checkbox" class="equipe"><?php echo $equipes_values['NOM']; ?> 
 									 		<ul class="liste_membres">
 									 			<?php
-									 				$membres = Equipes::getMembresEquipe($equipes_values['ID_EQUIPE']);
+									 				$membres = Equipe::getMembresEquipe($equipes_values['ID_EQUIPE']);
 													foreach ($membres as $membres_values) { ?>
 														<li><input type="checkbox" class="membre" name="<?php echo $membres_values['ID_PARTICIPANT']; ?>"><?php echo $membres_values['NOM'];?> <?php echo $membres_values['PRENOM']; ?></li>
 												<?php	}
