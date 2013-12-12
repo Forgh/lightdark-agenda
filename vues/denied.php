463f15535a98ee202bac72a174ccc637498efb21
kriszt4@gmail.com <kriszt4@gmail.com@68b665fa-9da4-a68a-76a0-4a3d28d06420>
2013-12-12 13:56:15 +0000

    Ajout d'un denied.php pour rediriger sur les erreurs Ajout d'une vue vue_reunion.php associée à son controleur

---
+++ vues/denied.php
@@ -0,0 +1,26 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+		<link rel="stylesheet" href="../css/style.css" content="html/css">
+		<META charset="utf-8"> 
+		<title>Acces impossible</title>
+	</head>
+
+<body>
+  
+<?php include("../include/header.php"); header("HTTP/1.0 404 Not Found"); ?>
+		
+		
+		<div id="bodycentered">
+			
+            <h2>Accès impossible</h2>
+            
+<p>La ressource à laquelle vous avez tenté d'accéder n'existe pas !</p>
+
+			
+		</div>
+
+</body>
+</html>
