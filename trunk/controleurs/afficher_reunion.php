6c26a09316da34cb54a99bbd707829fff71b2fee
mcflying.marty@gmail.com <mcflying.marty@gmail.com@68b665fa-9da4-a68a-76a0-4a3d28d06420>
2013-12-13 21:23:53 +0000

    maj afficher_reunion.php

--- trunk/controleurs/afficher_reunion.php
+++ trunk/controleurs/afficher_reunion.php
@@ -4,9 +4,10 @@
 	
 	session_start();
 	///////////////////////////////////
-	//$_SESSION['id'] = '2'; /*[DEBUG]*/
-	//$_GET['id'] = '1'; /*[DEBUG]*////
+	//$_SESSION['id'] = '2'; /*[DEBUG : id du type]*/
+	$_GET['id'] = '1'; /*[DEBUG : n° de la reunion]*////
 	////////////////////////////////
+
 	require('../modeles/Utilisateur.php');
 	require('../modeles/Reunion.php');
 	
@@ -62,13 +63,14 @@
 					<input type="submit" value="Confirmer">
 					</form>';
 					
-					/*variable contenant le formulaire vers suppr reunion*/
-					
+		
+				/*variable contenant le formulaire vers suppr reunion*/
+						
+		
 	}
 	else{
 		$confirmer = NULL;
 		$annuler = NULL;
-		$supprimer = NULL;
 	}
 		
 					
@@ -77,12 +79,18 @@
 				<input type="submit" value="Ajouter un rapport">
 				</form>';
 		
+	$supprimer = NULL; // si suppr possible alors il ne sera pas null
+
 		/*Si connecté, pas de rapport déclaré et est le chef de la réunion alors dépot de rapport possible*/		
 	if(isset($_SESSION['id']) && ($rapport =='<em>Aucun compte-rendu n\'a été déposé</em>') && Reunion::estChef($numReunion, $_SESSION['id']))
 		$addRapport = $ajouter;
 		
 		/*si chef -> $delete = $supprimer*/
-		
+			$supprimer = '<form method="get" action="afficher_reunion.php">
+					<input type="hidden" name="action" value="supprimer">
+					<input type="hidden" name="id" value="'.$numReunion.'">
+					<input type="submit" value="Supprimer">
+					</form>';
 		
 					
 	
