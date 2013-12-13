33d5cc883fc104c1e7a34c4534c09da9d882ef92
mcflying.marty@gmail.com <mcflying.marty@gmail.com@68b665fa-9da4-a68a-76a0-4a3d28d06420>
2013-12-13 22:35:18 +0000

    Verif si logué dans controleurs/afficher_reunion.php, controleurs/liste_reunion.php, controleurs/seek_organigramme.php

--- controleurs/liste_reunion.php
+++ controleurs/liste_reunion.php
@@ -1,10 +1,15 @@
 
-<?php if(!isset($_SESSION['id'])) session_start(); 
+<?php  session_start(); 
  
-include('../modeles/Reunion.php');
+ if(isset($_SESSION['id'])) {
+		include('../modeles/Reunion.php');
 
-	$liste = Reunion::afficher_les_reunions($_SESSION['id']);
-	
-	include('../vues/afficher_liste_reunions.php');
+		$liste = Reunion::afficher_les_reunions($_SESSION['id']);
+		
+		include('../vues/afficher_liste_reunions.php');
+	}else{
+		include('../vues/denied.php');
+	}
+
 ?>
 
