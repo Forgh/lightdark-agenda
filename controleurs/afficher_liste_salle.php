9939de7b2400bd1461c4eb47420ef4fd2904ece0
mickael.forgh@gmail.com <mickael.forgh@gmail.com@68b665fa-9da4-a68a-76a0-4a3d28d06420>
2013-12-14 18:55:14 +0000

    - Correction des erreurs, adaptation au modèle MVC

--- controleurs/afficher_liste_salle.php
+++ controleurs/afficher_liste_salle.php
@@ -1,13 +1,14 @@
 <?php
+	if(!isset($_SESSION['id'])) session_start();
+
 	include('../modeles/Salle.php');
-	$res = '<form method="post" action="../controleurs/afficher_future_salle.php">';
+	$res = '<form method="post" action="afficher_reunion_future_salle.php">';
 	$listSalle = Salle::getListeSalle();
 	foreach($listSalle as $p){
 
 		$s = Salle::getSalleById($p['NUM_SALLE']);
-		$res .= '<p><input type="radio" name="salle" value="' . $s->getNomSalle() . '">';
+		$res .= '<p><input type="radio" name="salle" value="' . $s->getNomSalle() . '">'.$s->getNomSalle().'</p>';
 	}
 	$res .= '<input type="submit" value="Valider"></form>';
-	
-	echo $res;
+	include('../vues/afficher_salles.php');
 ?>
